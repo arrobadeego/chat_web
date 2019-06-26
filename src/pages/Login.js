8c292ddc3ec6d6c01613d58749c7d69e2d2e0244
Diego Moreira <diegomptcc@hotmail.com>
2019-06-26 20:55:58 -0300

    put fields on Login component without style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,15 @@ class Login extends Component {
     render() {
         return(
             <div id="login">
-
+                <div>
+                    <input type="text" placeholder="e-mail"/>
+                    <input type="password" placeholder="password"/>
+                    <button>Sign in</button>
+                    <a>Forgot your password?</a>
+                    <span>If you don't have an account 
+                        <a> sign up now</a>
+                    </span>
+                </div>
             </div>
         );
     }
