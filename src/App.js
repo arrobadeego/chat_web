63ac76f1530f4237f89a331b697103c5a366d675
Diego Moreira <diegomptcc@hotmail.com>
2019-06-29 20:32:42 -0300

    create colors scss file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
+import './styles/colors.scss';
 
 function App() {
   return (
