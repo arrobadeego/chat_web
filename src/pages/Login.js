020999a420593ee140b96bf93294606a5ff696ca
Diego Moreira <diegomptcc@hotmail.com>
2019-06-29 19:13:20 -0300

    create Registration component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Select from 'react-select';
 import './Login.scss';
 import request from '../services/service';
---
+++ src/pages/Registration.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import './Login.scss';
+import request from '../services/service';
+import Cookies from 'js-cookie';
+
+class Registration extends Component {
+
+    state = {
+        name: null,
+        email: null,
+        password: null
+    }
+
+    render() {
+
+        return(
+            <div id="registration">
+                <div className="registration-form">
+                    <div className="registration-inputs">
+                        <input type="text" placeholder="Name" onChange={e => {this.setState({ name: e.target.value }) }}/>
+                        <input type="text" placeholder="E-mail" onChange={e => {this.setState({ email: e.target.value }) }}/>
+                        <input type="password" placeholder="Password" onChange={e => {this.setState({ password: e.target.value }) }}/>
+                        <input type="password" placeholder="Repeat your password" onChange={e => {this.setState({ password: e.target.value }) }}/>
+                    </div>
+                    <div className="registration-buttons">
+                        <button onClick={this.handleLogin}>Sign up</button>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Registration;
