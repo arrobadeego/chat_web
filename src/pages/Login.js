f31bfb3546d1e2e0cb860669fede5b1c77930bb0
Diego Moreira <diegomptcc@hotmail.com>
2019-06-29 19:00:42 -0300

    fix style Login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
 import './Login.scss';
 import request from '../services/service';
 import Cookies from 'js-cookie';
@@ -7,8 +8,14 @@ class Login extends Component {
 
     state = {
         email: null,
-        password: null
+        password: null,
+        selectedOption: null
     }
+
+      handleChange = selectedOption => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+      };
 
     handleLogin = () => {
         console.log(this.state);
@@ -22,18 +29,48 @@ class Login extends Component {
     }
 
     render() {
+
+        const options = [
+            { value: '1', label: 'Online' },
+            { value: '2', label: 'Away' },
+            { value: '3', label: 'Busy' },
+            { value: '4', label: 'Offline' },
+          ];
+
+
+        //   const customStyles = {
+        //     option: (provided, state) => ({
+        //       ...provided,
+        //     //   borderBottom: '1px dotted pink',
+        //     //   color: state.isSelected ? 'red' : 'blue',
+        //       padding: 20,
+        //     })
+        //   }
+
         return(
             <div id="login">
-                <div>
-                    <input type="text" placeholder="e-mail" onChange={e => {this.setState({ email: e.target.value }) }}/>
-                    <input type="password" placeholder="password" onChange={e => {this.setState({ password: e.target.value }) }}/>
-                    <button onClick={this.handleLogin}>Sign in</button>
-                    <a>Forgot your password?</a>
-                    <span>If you don't have an account 
-                        <a> sign up now</a>
-                    </span>
+                <div className="login-form">
+                    <div className="login-inputs">
+                        <input type="text" placeholder="E-mail" onChange={e => {this.setState({ email: e.target.value }) }}/>
+                        <input type="password" placeholder="Password" onChange={e => {this.setState({ password: e.target.value }) }}/>
+                    </div>
+
+                    <Select
+                        placeholder="Sign in as"
+                        value={this.state.selectedOption}
+                        options={options}
+                        onChange={this.handleChange}
+                    />
+                    <div className="login-buttons">
+                        <button onClick={this.handleLogin}>Sign in</button>
+                        <a>Forgot your password?</a>
+                        <span>If you don't have an account 
+                            <a> sign up now</a>
+                        </span>
+                    </div>
                 </div>
             </div>
+
         );
     }
 }
