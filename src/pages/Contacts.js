30e8d5d83dd3cdc8ccc0e7419b64dea7ff256511
Diego Moreira <diegomptcc@hotmail.com>
2019-06-30 14:45:49 -0300

    fix link on Login and add link on Registration

---
+++ src/pages/Contacts.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Contacts.scss';
+
+class Contacts extends Component {
+
+    render() {
+
+        return(
+            <div id="contacts">
+                
+            </div>
+        );
+    }
+}
+
+export default Contacts;
