8231c1b9cac4cf8c724feda1e977fb8354bb2345
Diego Moreira <diegomptcc@hotmail.com>
2019-07-06 20:06:34 -0300

    create Main component

---
+++ src/pages/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import request from '../services/service';
+import './Contacts.scss';
+
+class Main extends Component {
+
+    state = {
+        sidebarOpen: true,
+        contacts: []
+    };
+
+    componentDidMount = () => {
+        request.Contacts.getContacts().then( res => {
+            if(res.status === 200){
+                console.log(res.data.contacts);
+                this.setState({contacts: res.data.contacts})
+            }
+        }).catch(function (response) {
+            this.setState({ errors: { className: "errors" } });
+            console.log(response);
+        });
+    }
+
+    onSetSidebarOpen = (open) => {
+        this.setState({ sidebarOpen: open });
+    }
+
+    render() {
+
+        const contactsList = this.state.contacts.map(contact => (<ul><li>{contact.name}</li></ul>))
+
+        return(
+            <div id="main">
+                
+            </div>
+        );
+    }
+}
+
+export default Main;
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Registration from './pages/Registration';
-import Contacts from './pages/Contacts';
+import Main from './pages/Main';
 
 function Routes() {
     return(
         <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/registration" exact component={Registration} />
-            <Route path="/contacts" exact component={Contacts} />
+            <Route path="/main" exact component={Main} />
         </Switch>
     );
 }
