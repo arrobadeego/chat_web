8645502549fbfcd9310bf2a79ff7ef8ef9146c47
Diego Moreira <diegomptcc@hotmail.com>
2019-07-11 18:46:38 -0300

    create request to get profile on service

--- src/services/service.js
+++ src/services/service.js
@@ -19,6 +19,7 @@ const request = {
 
 const Auth = {
     login: (email, password) => request.post(`login`, { email: email, password: password }, HEADERS),
+    profile: () => request.get(`profile`, HEADERS),
     registration: (name, email, password) => request.post(`registration`, { name, email, password }, HEADERS)
 };
 
