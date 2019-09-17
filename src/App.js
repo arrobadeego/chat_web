f76b7bceb4890d21f3ced95aff3bb66676fe5686
Diego Moreira <diegomptcc@hotmail.com>
2019-09-17 15:45:10 -0300

    [REFACTOR] configure reactotron

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import Routes from './routes';
+import store from './store';
+
 import history from './services/history';
 
 import GlobalStyle from './styles/global';
 
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-            <ToastContainer autoClose={3000} />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+                <ToastContainer autoClose={3000} />
+            </Router>
+        </Provider>
     );
 }
 
