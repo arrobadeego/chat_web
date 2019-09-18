3b04f01f12479a5667faf4a7d380465193b2b0a3
Diego Moreira <diegomptcc@hotmail.com>
2019-09-18 10:04:08 -0300

    create saga sign up request

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,19 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import signUpSuccess from './actions';
+
+export function* signUp({ payload }) {
+    const { name, email, password } = payload;
+
+    const response = yield call(api.post, 'registration', {
+        name,
+        email,
+        password,
+    });
+
+    const { token, user } = response.data;
+
+    yield put(signUpSuccess(token, user));
+}
+
+export default all([takeLatest('@auth/SIGN_UP_REQUEST', signUp)]);
