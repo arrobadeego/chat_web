d855b4616dfa7f5efec5dd92993306b507fb297c
Diego Moreira <diegomptcc@hotmail.com>
2019-09-18 10:57:30 -0300

    put try catch on sign up request

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -8,19 +8,24 @@ import api from '../../../services/api';
 import { signUpSuccess, signFailure } from './actions';
 
 export function* signUp({ payload }) {
-    const { name, email, password } = payload;
+    try {
+        const { name, email, password } = payload;
 
-    const response = yield call(api.post, 'registration', {
-        name,
-        email,
-        password,
-    });
+        const response = yield call(api.post, 'registration', {
+            name,
+            email,
+            password,
+        });
 
-    const { token, user } = response.data;
+        const { token, user } = response.data;
 
-    yield put(signUpSuccess(token, user));
+        yield put(signUpSuccess(token, user));
 
-    history.push('/dashboard');
+        history.push('/dashboard');
+    } catch (err) {
+        toast.error('Register failure, check your info');
+        yield put(signFailure());
+    }
 }
 
 export default all([takeLatest('@auth/SIGN_UP_REQUEST', signUp)]);
