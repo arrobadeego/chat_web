ff81288c9bf76cad2a4597ef00e2599889f348fb
Diego Moreira <diegomptcc@hotmail.com>
2019-09-18 09:40:22 -0300

    configure store

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
+import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMonitor =
-    process.env.NODE_ENV === 'development'
-        ? console.tron.createSagaMonitor()
-        : null;
+const sagaMiddleware = createSagaMiddleware();
 
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+const middlewares = [sagaMiddleware];
 
-const enhancer =
-    process.env.NODE_ENV === 'development'
-        ? compose(
-              console.tron.createEnhancer(),
-              applyMiddleware(sagaMiddleware)
-          )
-        : applyMiddleware(sagaMiddleware);
-
-const store = createStore(rootReducer, enhancer);
+const store = createStore(rootReducer, middlewares);
 
 sagaMiddleware.run(rootSaga);
 
