bf4ca8b94fb3f293be9d7443498bdbff0af26597
Diego Moreira <diegomptcc@hotmail.com>
2019-09-18 12:39:26 -0300

    [REFACTOR] create sign in component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { signUpRequest } from '../../store/modules/auth/actions';
+
+import { Container } from './styles';
+
+const schema = Yup.object().shape({
+    email: Yup.string().required('E-mail is required'),
+    password: Yup.string().required('Password is required'),
+});
 
 export default function SignIn() {
-    return <h1>Login</h1>;
+    const dispatch = useDispatch();
+
+    function handleSubmit({ name, email, password }) {
+        dispatch(signUpRequest(name, email, password));
+    }
+
+    return (
+        <Container>
+            <span>Zip Zop</span>
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="email" type="email" placeholder="E-mail" />
+                <Input name="password" type="password" placeholder="Password" />
+
+                <button type="submit">Sign in</button>
+                <Link to="/">Create account</Link>
+            </Form>
+        </Container>
+    );
 }
--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -9,12 +9,14 @@ export default function auth(state = INITIAL_STATE, action) {
                 draft.loading = true;
                 break;
             }
+
             case '@auth/SIGN_UP_SUCCESS': {
                 draft.token = action.payload.token;
                 draft.signed = true;
                 draft.loading = false;
                 break;
             }
+
             case '@auth/SIGN_FAILURE': {
                 draft.loading = false;
                 break;
