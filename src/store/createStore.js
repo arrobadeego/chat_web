546089acb97b757531b6009415662d6fbd81d81c
Diego Moreira <diegomptcc@hotmail.com>
2019-09-18 09:48:00 -0300

    configure saga monitor on store

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,13 +1,5 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 export default (reducers, middlewares) => {
-    const enhancer =
-        process.env.NODE_ENV === 'development'
-            ? compose(
-                  console.tron.createEnhancer(),
-                  applyMiddleware(...middlewares)
-              )
-            : applyMiddleware(...middlewares);
-
-    return createStore(reducers, enhancer);
+    return createStore(reducers, middlewares);
 };
