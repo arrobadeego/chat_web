2c3487900e75212fd2c64427eb1252c2c949b668
Diego Moreira <diegomptcc@hotmail.com>
2019-09-19 15:25:43 -0300

    create contact

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Header } from './styles';
+import { Container, Header, Content } from './styles';
 import Dropdown from '../Dropdown';
 
 export default function Contacts() {
@@ -14,6 +14,20 @@ export default function Contacts() {
 
                 <Dropdown />
             </Header>
+            <Content>
+                <img
+                    src="https://api.adorable.io/avatars/50/abott@adorable.png"
+                    alt=""
+                />
+
+                <div>
+                    <div>
+                        <span>Jobson</span>
+                        <span>Pessoal</span>
+                    </div>
+                    <span>21:59</span>
+                </div>
+            </Content>
         </Container>
     );
 }
--- src/components/Contacts/styles.js
+++ src/components/Contacts/styles.js
@@ -22,3 +22,43 @@ export const Header = styled.header`
         border: 3px solid red;
     }
 `;
+
+export const Content = styled.div`
+    background: MediumSpringGreen;
+    height: 50px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 20px;
+
+    img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        border: 3px solid green;
+    }
+
+    div {
+        width: 100%;
+        margin-left: 5px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        justify-items: center;
+
+        div {
+            display: flex;
+            flex-direction: column;
+
+            span:first-child {
+                font-weight: bolder;
+                color: purple;
+            }
+
+            span:last-child {
+                font-style: italic;
+                color: gray;
+            }
+        }
+    }
+`;
