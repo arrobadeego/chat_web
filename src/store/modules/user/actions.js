2f0e644c76511d16ad77664234b75666f9a1eade
Diego Moreira <diegomptcc@hotmail.com>
2019-09-22 17:29:14 -0300

    create update's profile actions

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -4,3 +4,23 @@ export function signUpRequest(name, email, password) {
         payload: { name, email, password },
     };
 }
+
+export function updateProfileRequest(data) {
+    return {
+        type: '@user/UPDATE_PROFILE_REQUEST',
+        payload: { data },
+    };
+}
+
+export function updateProfileSuccess(profile) {
+    return {
+        type: '@user/UPDATE_PROFILE_SUCCESS',
+        payload: { profile },
+    };
+}
+
+export function updateProfileFailure() {
+    return {
+        type: '@user/UPDATE_PROFILE_FAILURE',
+    };
+}
