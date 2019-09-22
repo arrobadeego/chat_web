1f0e3a957e213dcc9a4007850ca4beffa0b7ce8b
Diego Moreira <diegomptcc@hotmail.com>
2019-09-22 17:21:28 -0300

    create function to update profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import { MdKeyboardBackspace } from 'react-icons/md';
 import * as Yup from 'yup';
 
 // import Sidemenu from '../Sidemenu';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 import AvatarInput from './AvatarInput';
 
 import { Header, Content } from './styles';
@@ -25,8 +26,13 @@ const schema = Yup.object().shape({
     ),
 });
 
-export default function Profile({handleClickAction}) {
+export default function Profile({ handleClickAction }) {
     const profile = useSelector(state => state.user.profile);
+    const dispatch = useDispatch();
+
+    function handleSubmit(data) {
+        dispatch(updateProfileRequest(data));
+    }
 
     return (
         <>
@@ -38,9 +44,14 @@ export default function Profile({handleClickAction}) {
                 <span>Profile</span>
             </Header>
             <Content>
-                <Form initialData={profile} schema={schema}>
+                <Form
+                    initialData={profile}
+                    schema={schema}
+                    onSubmit={handleSubmit}
+                >
                     <AvatarInput name="avatar" />
                     <Input name="name" placeholder="Name" />
+
                     <Input name="email" type="email" placeholder="E-mail" />
                     <Input
                         name="oldPassword"
@@ -54,7 +65,7 @@ export default function Profile({handleClickAction}) {
                     />
 
                     <Input
-                        type="confirmPassword"
+                        type="password"
                         name="passwordConfirmation"
                         placeholder="Confirm your new password"
                     />
