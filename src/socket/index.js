28546fe79429c8395188d979d397019fb8bcc214
Diego Moreira <diegomptcc@hotmail.com>
2019-09-28 22:12:13 -0300

    socket config

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,10 +1,19 @@
 import io from 'socket.io-client';
+import { useDispatch } from 'react-redux';
 
 const socket = io('http://localhost:3333');
 
 socket.on('connect', data => {
     console.log('Socket connected');
+    const dispatch = useDispatch();
 
+    switch (data.type) {
+        case 'notifyInvite': {
+            
+        }
+
+        default:
+    }
     // socket.on('teste', data => {
     //     setData(data.name);
     // });
