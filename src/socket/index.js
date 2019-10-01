d7d17f0409ce0428c4789b289a2d520f06f918f4
Diego Moreira <diegomptcc@hotmail.com>
2019-09-30 21:18:27 -0300

    receive socket NOTIFY_INVITE and update profile object

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,21 +5,13 @@ import * as types from '../constants/ActionTypes';
 
 const setupSocket = dispatch => {
     const socket = io('http://localhost:3333');
-
     socket.on('connect', event => {
         console.log('Socket connected');
-        console.log(event);
 
-        const data = JSON.parse(event.data);
-        switch (data.type) {
-            case types.NOTIFY_INVITE:
-                dispatch(notifyInvite(data));
-                break;
-            default:
-                break;
-        }
+        socket.on('NOTIFY_INVITE', data => {
+            dispatch(notifyInvite(data));
+        });
     });
-
     return socket;
 };
 
